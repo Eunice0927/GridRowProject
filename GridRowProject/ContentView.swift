620f5a3d7d83e1571e5c0dd2b1366ebe3ffe7116
Eunice <jes.e.0927@gmail.com>
2023-11-13 19:23:03 +0900

    ✨ Feat: Set LazyVGrid
    Components:
    - gridStatus[Int: Bool] => button이 클릭되면 index key에 해당되는 value가 false로 바뀐다.
    - randomInts: [Int] => 1-20까지의 정수가 순서가 섞여 들어간다.
    - color: [Color] => Grid(카드)의 배경색이 랜덤으로 나온다.
    
    body:
    LazyVGrid를 GridItem이 4개씩 출력되도록 설정
    ForEach로 randomInts의 원소 수 만큼 버튼이 만들어진다.
    randomInts의 원소 value가 index가 되어 버튼에 Label로 출력된다.
    버튼의 배경색은 index를 color 배열의 갯수로 나눈 나머지에 해당하는 숫자가 index가 되어 color값을 가져온다.

--- GridRowProject/ContentView.swift
+++ GridRowProject/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gridStatus = [0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true, 10: true, 11: true, 12: true, 13: true, 14: true, 15: true, 16: true, 17: true, 18: true, 19: true, 20: true]
+    @State private var randomInts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].shuffled()
+    @State private var color: [Color] = [.yellow, .green, .purple].shuffled()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        LazyVGrid(columns: Array(repeating: GridItem(.adaptive(minimum: 50)), count: 4)) {
+            ForEach(randomInts, id: \.self) { index in
+                Button {
+                    
+                } label: {
+                    Text("\(index)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                }
+                .background(color[index % color.count])
+                .foregroundStyle(.white)
+                .cornerRadius(8)
+                .font(.largeTitle)
+            }
         }
         .padding()
     }
