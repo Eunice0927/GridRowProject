0d0d7481a0880c7a5aa1363a9291b29e0d2b6b68
Eunice <jes.e.0927@gmail.com>
2023-11-13 19:00:13 +0900

    Initial Commit

---
+++ GridRowProject/GridRowProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridRowProjectApp.swift
+//  GridRowProject
+//
+//  Created by Eunsu JEONG on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GridRowProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
