fd67d0f4606fbd1ed4477c93704da6f054cb9e2b
Eunice <jes.e.0927@gmail.com>
2023-11-13 19:57:24 +0900

    ✨ Feat: Add game end view
    1부터 순서대로 눌러야 하는 게임 규칙을 어기면 게임 실패 화면이 뜬다.
    Zstack을 활용하여 기존 Grid 위에 실패 화면이 보여지도록 한다.
    
    Componets:
    - showEndView: Bool => endView가 화면에 그려질지 여부를 결정한다.
    - title: String => endView에 보여질 텍스트이다.

--- GridRowProject/ContentView.swift
+++ GridRowProject/ContentView.swift
@@ -12,37 +12,53 @@ struct ContentView: View {
     @State private var randomInts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].shuffled()
     @State private var color: [Color] = [.yellow, .green, .purple].shuffled()
     @State private var previousIndex = 1
+    @State private var showEndView: Bool = false
+    @State private var title = ""
     
     var body: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(.adaptive(minimum: 50)), count: 4)) {
-            ForEach(randomInts, id: \.self) { index in
-                if gridStatus[index] ?? true {
-                    Button {
-                        if index > previousIndex {
-
-                        } else {
-                            previousIndex = index + 1
+        ZStack {
+            LazyVGrid(columns: Array(repeating: GridItem(.adaptive(minimum: 50)), count: 4)) {
+                ForEach(randomInts, id: \.self) { index in
+                    if gridStatus[index] ?? true {
+                        Button {
+                            if index > previousIndex {
+                                title = "실패😭"
+                                showEndView = true
+                            } else {
+                                previousIndex = index + 1
+                                self.gridStatus[index] = false
+                            }
+                        } label: {
+                            Text("\(index)")
+                                .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        }
+                        .background(color[index % color.count])
+                        .foregroundStyle(.white)
+                        .cornerRadius(8)
+                        .font(.largeTitle)
+                    } else {
+                        Button("") {
                             self.gridStatus[index] = false
                         }
-                    } label: {
-                        Text("\(index)")
-                            .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(.clear)
+                        .cornerRadius(8)
                     }
-                    .background(color[index % color.count])
-                    .foregroundStyle(.white)
-                    .cornerRadius(8)
-                    .font(.largeTitle)
-                } else {
-                    Button("") {
-                        self.gridStatus[index] = false
-                    }
-                    .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
-                    .background(.clear)
-                    .cornerRadius(8)
                 }
             }
+            .padding()
+            
+            if showEndView {
+                VStack {
+                    Text(title)
+                        .font(.system(size: 80))
+                }
+                .frame(width: 370, height: 500)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .foregroundStyle(.black)
+            }
         }
-        .padding()
         
         Button {
             reset()
@@ -59,6 +75,7 @@ struct ContentView: View {
         gridStatus = [0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true, 10: true, 11: true, 12: true, 13: true, 14: true, 15: true, 16: true, 17: true, 18: true, 19: true, 20: true]
         randomInts = randomInts.shuffled()
         color = color.shuffled()
+        previousIndex = 1
     }
 }
 
